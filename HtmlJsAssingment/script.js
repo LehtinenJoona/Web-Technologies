8f1045b4a5b313595ac8f7216124fc88346e9da5
mire <hmire02@xamk.fi>
2023-09-19 22:03:13 +0300

    Add first assignment

---
+++ HtmlJsAssingment/script.js
@@ -0,0 +1,29 @@
+// Add new todo task to todoList
+function addTodo() {
+    // TODO: Get the value from todoInput element
+
+    // TODO: Check that input value was provided
+
+    // TODO: Get todoList element from html and assign it to variable
+
+    // TODO: Create new li element and assign it to variable
+
+    // TODO: Assing the todoInput value to the textContent
+
+    // TODO: Create completed button to set the task to done, createElement
+
+    // TODO: Add text to the completed button using textContent
+
+    // TODO: Add onclick method to the completed button, that changes the background color of the todo item
+    // Below is one way to change the background color using ternary logic
+    // todoListItem.style.backgroundColor = todoListItem.style.backgroundColor == "green" ? "" : "green";
+
+    // TODO: Add created done button to new todo list item
+
+    // TODO: Add new todo list item to todoList
+
+    // TODO: Set todoInput value to empty string
+}
+
+// Extra assingment -> try to find a way to remove all completed todo items
+// hint -> you might need to use additional list variable
